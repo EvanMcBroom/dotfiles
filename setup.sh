6a7143390535da63e7358dc53fe9d26d2d43e299
Evan McBroom <15305789+EvanMcBroom@users.noreply.github.com>
2018-05-30 15:38:58 -0400

    Fixed some bugs in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-error() {
-  echo "$1"
-  exit
-}
-
 # The script's directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Check for git and tmux
-git --version 2>&1 >/dev/null
-[ $? -ne 0 ] && error "Install git"
-tmux -V 2>&1 >/dev/null
-[ $? -ne 0 ] && error "Install git"
+# Install git, tmux, and zsh
+apt-get -y install --upgrade git
+apt-get -y install --upgrade tmux
+apt-get -y install --upgrade zsh
 
 # Install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
@@ -20,23 +14,23 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install pathogen bundles
 pushd ~/.vim/bundle
-git -C nerdtree pull || git clone https://github.com/scrooloose/nerdtree
-git -C syntastic pull || git clone https://github.com/scrooloose/syntastic
-git -C vim-airline pull || git clone https://github.com/bling/vim-airline
-git -C vim-airline-themes pull || git clone https://github.com/vim-airline/vim-airline-themes
-git -C vim-colors-solarized pull || git clone https://github.com/altercation/vim-colors-solarized
-git -C vim-fugitive pull || git clone https://github.com/tpope/vim-fugitive
+git -C nerdtree pull 2>/dev/null || git clone https://github.com/scrooloose/nerdtree
+git -C syntastic pull 2>/dev/null || git clone https://github.com/scrooloose/syntastic
+git -C vim-airline pull 2>/dev/null || git clone https://github.com/bling/vim-airline
+git -C vim-airline-themes pull 2>/dev/null || git clone https://github.com/vim-airline/vim-airline-themes
+git -C vim-colors-solarized pull 2>/dev/null || git clone https://github.com/altercation/vim-colors-solarized
+git -C vim-fugitive pull 2>/dev/null || git clone https://github.com/tpope/vim-fugitive
 popd
 
 # Install oh-my-zsh
-[ ! -d "~/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed -e 's/env zsh//g')" 
 cp $DIR/evanmcbroom.zsh-theme .oh-my-zsh/custom/evanmcbroom.zsh-theme # Add my oh-my-zsh theme
 
 # Get current dircolors-solarized
 curl -LSso ~/.dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
 
 # Install dotfiles
-pushd # ~/
+pushd ~/
 [ -f .bash_profile ] && mv .bash_profile .bash_profile.bck
 cp $DIR/.bash_profile .bash_profile
 [ -f .bashrc ] && mv .bashrc .bashrc.bck
@@ -50,5 +44,4 @@ cp $DIR/.tmux.conf .tmux.conf
 cp $DIR/.vimrc .vimrc
 [ -f .zshrc ] && mv .zshrc .zshrc.bck
 cp $DIR/.zshrc .zshrc
-source .zshrc
 popd
