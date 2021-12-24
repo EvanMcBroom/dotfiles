b9499e77756ce51b488afecaba483fece97b45b4
Evan McBroom <15305789+EvanMcBroom@users.noreply.github.com>
2021-12-24 10:36:49 -0500

    Finished the setup script

--- setup.sh
+++ setup.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
 
-# The script's directory
+# The directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Script name
+SCRIPT_NAME=`basename "$0"`
 
-# Install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+help_message () {
+    cat << EOF
+Commands:
+  validate  Check whether the dotfiles were installed
+  plan      Show changes required to install dotfiles
+  apply     Create a backup then install dotfiles
+  destroy   Restore dotfiles from the backup
+EOF
+    exit 1
+}
 
-# Install pathogen bundles
-pushd ~/.vim/bundle
-git -C nerdtree pull 2>/dev/null || git clone https://github.com/scrooloose/nerdtree
-git -C syntastic pull 2>/dev/null || git clone https://github.com/scrooloose/syntastic
-git -C vim-airline pull 2>/dev/null || git clone https://github.com/bling/vim-airline
-git -C vim-airline-themes pull 2>/dev/null || git clone https://github.com/vim-airline/vim-airline-themes
-git -C vim-colors-solarized pull 2>/dev/null || git clone https://github.com/altercation/vim-colors-solarized
-git -C vim-fugitive pull 2>/dev/null || git clone https://github.com/tpope/vim-fugitive
-popd
-
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed -e 's/env zsh//g')" 
-cp $DIR/evanmcbroom.zsh-theme .oh-my-zsh/custom/evanmcbroom.zsh-theme # Add my oh-my-zsh theme
-
-# Get current dircolors-solarized
-curl -LSso ~/.dircolors https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
-
-# Install dotfiles
-pushd ~/
-[ -f .aliases ] && mv .aliases .aliases.bck
-cp $DIR/.aliases .aliases
-[ -f .bash_profile ] && mv .bash_profile .bash_profile.bck
-cp $DIR/.bash_profile .bash_profile
-[ -f .bashrc ] && mv .bashrc .bashrc.bck
-cp $DIR/.bashrc .bashrc
-[ -f .gitconfig ] && mv .gitconfig .gitconfig.bck
-cp $DIR/.gitconfig .gitconfig
-[ -f .gitignore_global ] && mv .gitignore_global .gitignore_global.bck
-cp $DIR/.gitignore_global .gitignore_global
-[ -f .tmux.conf ] && mv .tmux.conf .tmux.conf.bck
-cp $DIR/.tmux.conf .tmux.conf
-[ -f .vimrc ] && mv .vimrc .vimrc.bck
-cp $DIR/.vimrc .vimrc
-[ -f .zshrc ] && mv .zshrc .zshrc.bck
-cp $DIR/.zshrc .zshrc
-popd
+if [ $# -eq 0 ]; then # No arguments
+    help_message
+elif [ $1 = "apply" ]; then
+    mkdir -p ~/.dotfiles
+    echo "$SCRIPT_NAME: Backing up..."
+    for file in $(find ~ -maxdepth 1 -type f -name ".*"); do
+        echo "~/$(basename $file) -> ~/.dotfiles/$(basename $file).bck"
+        mv $file "$HOME/.dotfiles/$(basename $file).bck"
+    done
+    printf "\n$SCRIPT_NAME: Adding...\n"
+    for file in $(find $DIR -maxdepth 1 -type f -name ".*" -not -name ".gitattributes"); do
+        echo "$(basename $file) -> ~/$(basename $file)"
+        cp $file ~
+    done
+elif [ $1 = "destroy" ]; then
+    if [ -d "$HOME/.dotfiles" ]; then
+        find ~ -maxdepth 1 -type f -name ".*" | xargs -i{} rm {}
+        for file in $(find ~/.dotfiles -maxdepth 1 -type f -name ".*.bck"); do
+            mv $file "$HOME/$(basename ${file::-4})"
+        done
+        rmdir "$HOME/.dotfiles"
+    else
+        echo "The backup ~/.dotfiles directory does not exist."
+    fi
+elif [ $1 = "plan" ]; then
+    original=$(find ~ -maxdepth 1 -type f -name ".*")
+    new=$(find $DIR -maxdepth 1 -type f -name ".*" -not -name ".gitattributes")
+    printf "\nPlan: $(echo $original | wc -w) to backup, $(echo $new | wc -w) to add\n"
+    printf "\n$SCRIPT_NAME: Backing up...\n"
+    for file in $original; do echo "~/$(basename $file) -> ~/.dotfiles/$(basename $file).bck"; done
+    printf "\n$SCRIPT_NAME: Adding...\n"
+    for file in $new; do echo "$(basename $file) -> ~/$(basename $file)"; done
+elif [ $1 = "validate" ]; then
+    valid="true"
+    # Execute the loop in the same shell to be about to set the "valid" variable
+    # https://stackoverflow.com/a/16855194/11039217
+    while read file; do
+        file2="$HOME/$(basename $file)"
+        if [ ! -f $file2 ]; then
+            echo "Error: Missing dotfile $(basename $file)" && valid="false" && break
+        elif ! cmp -s $file $file2; then
+            echo "Error: The $(basename $file) dotfiles do not match" && valid="false" && break
+        fi
+    done < <(find $DIR -maxdepth 1 -type f -name '.*' -not -name '.gitattributes')
+    [ $valid = "true" ] && echo "Success! The configuration is valid."
+else
+    help_message
+fi
